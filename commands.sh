67c832b075d3fb84d91b33e4e7468c3f8edf6ab3
iamtejasmane <tejasmane485@gmail.com>
2022-06-24 23:31:35 +0530

    commands updated

--- commands.sh
+++ commands.sh
@@ -65,14 +65,70 @@ cat file1 > file2 >> file3 # add content to file
 
 less filename # shows content stored inside file with nice UI
 
-tail # best for log files 
+tail # best for log files
+# -f opens the file at the end and watches for file changes.
 
 wc -l -w -c file.txt # gives information about input receives via pipes
 # -l count lines; -w words; -c bytes
 
-grep # Global Regular Expression; used to search in files or combine it with pipe to filter o/p
-# of other command. 
+grep # Global Regular Expression; used to search in files or 
+# combine it with pipe to filter output of other command. 
 
-grep -n cy.logout() cyressTest.js # find occurrences in the file
+grep -n cy.logout() cyressTest.js # find occurrences in the file; 
+# -n shows the line number
+# -i case insensitive (default case sensitive)
 
+diff <file1> <file2> # give the differences between file1 and file2
+# -y compares two files line by line; 
+# -u displays differences between versions like Git
+# -r to compare directories
 
+chown <owner> <file> # change the owner of the file
+chown -R <owner> <file> # change the owner of the files recursively
+chown <owner>:<group> <file> # change the owner of a group
+# e.g chown yamlfile:users test.txt
+
+# alternative:
+chgrp <group> <file>
+
+chmod # change permissions of files 
+# symbolic arguments
+a stands for all
+u stands for user
+g stands for group
+o stands for others
+
+"+ or - to add or remove permissions"
+"r,w,x"
+
+chmod a+r <file> # read allowed to all
+chmod og+rw <file> # read write allowed to others and groups
+chmod o-rwx <file> # others are not allowed to read write and execute
+
+# numeric arguments
+1 if has execution permission
+2 if has write permission
+4 if has read permission
+
+# This gives us 4 combinations:
+0 no permissions
+1 can execute
+2 can write
+3 can write, execute
+4 can read
+5 can read, execute
+6 can read, write
+7 can read, write and execute
+
+"Three permissions for all three groups"
+
+chmod 777 <filename>
+chmod 755 <filename>
+chmod 644 <filename>
+
+umask -S # shows the default permissions
+
+du # calculates size of directory as a whole in bytes
+# -m megabytes; -g gigabytes; -h human readable notation
+
+df # get disk usage information; -h human readable; 
