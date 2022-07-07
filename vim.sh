9bab462270be57edc5e0b5d3d226da846256a0c7
iamtejasmane <tejasmane485@gmail.com>
2022-07-07 10:58:44 +0530

    vim commands added

---
+++ vim.sh
@@ -0,0 +1,100 @@
+# Vim Tutorial
+
+# vim is working in the different modes
+i - insert mode - text before the cursor
+a - insert mode - text after the cursor
+o - insert mode - text new line
+
+# capitals
+I - insert mode - text starts from start of the line
+A - insert mode - text starts from the end of the line
+O - insert mode - text start from above the existing line
+
+# set number - sets the line numbers
+
+# movement keys
+h left
+j down 
+k up
+l right
+
+# to set relative line numbers
+set relativenumber
+
+# to activate mouse
+set mouse=a
+
+# path of the vim dir
+vi ~/.vimrc
+
+# undo
+u
+
+# redo 
+Ctrl + R
+
+# visual mode 
+v - "use arrows to select the text"
+
+# delete
+d 
+
+# copy
+y
+
+# paste 
+p 
+# to paset 4 times
+4p
+
+# to edit 
+c
+
+# to replace 
+r
+
+# to jump word by word
+w
+
+# to jump backward word by word
+b
+
+# delete a word
+dw
+
+# delet till end of the line
+d$ 
+
+# delete till start of the line from current word 
+d0
+
+# copy a word when you are middle of the word
+yiw
+
+# change the word 
+ciw
+
+# delete till a specific character
+'print("some text here") - di"'
+
+# change till a specific char
+ci<char>
+
+# jump begining of the file 
+gg
+
+# jump end of the file 
+Shift + g
+
+# to select multiple lines 
+Shift + v
+
+# to indent the lines
+=
+
+# searching 
+/<keyword>
+n - goes to next word
+N - goes to previous word
+
+
